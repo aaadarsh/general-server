c27c6d9d9561d2ba77318c7e65011114e432a89e
Adarsh <adarsh@Adarsh-MacPro.local>
2020-06-17 17:58:27 +0530

    commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var express = require ('express');
+var mongoose = require ('mongoose');
+var bodyparser = require ('body-Parser');
+var cors = require ('cors');
+var path = require('path');
+
+var app = express();
+const route = require ('./route/routes');
+
+//mongodb connection
+
+mongoose.connect("mongodb://localhost:27017/shoppinglist", { useNewUrlParser: true });
+
+mongoose.connection.on('connected', ()=>{
+    console.log('connected to mongoDb');
+});
+mongoose.connection.on('error', ()=>{
+    console.log(error);
+});
+
+//home route
+app.get('/', (req, res)=>{
+    res.send('Home page- Backend');
+});
+
+//integration
+/*
+app.use(express.static(path.join(__dirname + 'dist')));
+
+app.get('*', function (req, res, next) {
+    res.sendFile(path.join(__dirname, 'dist/shopping-list/index.html'));
+    
+});
+*/
+
+app.use(cors());
+app.use(bodyparser.json());
+
+app.use('/api', route);  
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log('Server started at port: ' + port);
+})
---
+++ model/shoppingItem.js
@@ -0,0 +1,27 @@
+//Idefining Schema to check the Item name, Item quantity and telling whether the item has been brought or not
+
+
+const mongoose = require('mongoose');
+
+const shoppingItemSchema = mongoose.Schema({
+           
+    itemName: {
+            type: String,
+            required: true
+            },
+
+    itemQuantity: {
+
+        type: Number,
+        required: true
+    },
+
+    itemBought: {
+        type: Boolean,
+        required: true
+    }
+    // timestamp: true --> it autometically creates, created at and updated at fields, it can be used instead of date object 
+
+});
+
+const item = module.exports = mongoose.model('item', shoppingItemSchema);
