6dbc5cf46ccb94081cde455164ace8e2d1685ccf
aaadarsh <38029416+aaadarsh@users.noreply.github.com>
2022-09-27 09:23:02 +0530

    model commented to make it simple furthermore

--- model/shoppingItem.js
+++ model/shoppingItem.js
@@ -1,27 +1,27 @@
-//Idefining Schema to check the Item name, Item quantity and telling whether the item has been brought or not
+// //Idefining Schema to check the Item name, Item quantity and telling whether the item has been brought or not
 
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const shoppingItemSchema = mongoose.Schema({
+// const shoppingItemSchema = mongoose.Schema({
            
-    itemName: {
-            type: String,
-            required: true
-            },
+//     itemName: {
+//             type: String,
+//             required: true
+//             },
 
-    itemQuantity: {
+//     itemQuantity: {
 
-        type: Number,
-        required: true
-    },
+//         type: Number,
+//         required: true
+//     },
 
-    itemBought: {
-        type: Boolean,
-        required: true
-    }
-    // timestamp: true --> it autometically creates, created at and updated at fields, it can be used instead of date object 
+//     itemBought: {
+//         type: Boolean,
+//         required: true
+//     }
+//     // timestamp: true --> it autometically creates, created at and updated at fields, it can be used instead of date object 
 
-});
+// });
 
-const item = module.exports = mongoose.model('item', shoppingItemSchema);
+// const item = module.exports = mongoose.model('item', shoppingItemSchema);
